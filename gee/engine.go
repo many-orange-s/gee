411cd376805bc1ce5a370a3108938b7bd9672def
many-orange-s <84865020+many-orange-s@users.noreply.github.com>
2021-11-12 20:40:29 +0800

    ADD:Group的引入

--- gee/engine.go
+++ gee/engine.go
@@ -6,24 +6,48 @@ import (
 
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
+
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: NewRouter()}
+	engine := &Engine{router: NewRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRouter(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRouter(method, pattern, handler)
+func (group *RouterGroup) Group(pattern string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + pattern,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (engine *Engine) Get(pattern string, handler HandlerFunc) {
-	engine.addRouter("Get", pattern, handler)
+func (group *RouterGroup) addRouter(method string, path string, handler HandlerFunc) {
+	pattern := group.prefix + path
+	group.engine.router.addRouter(method, pattern, handler)
 }
 
-func (engine *Engine) Post(pattern string, handler HandlerFunc) {
-	engine.addRouter("Post", pattern, handler)
+func (group *RouterGroup) Get(pattern string, handler HandlerFunc) {
+	group.addRouter("Get", pattern, handler)
+}
+
+func (group *RouterGroup) Post(pattern string, handler HandlerFunc) {
+	group.addRouter("Post", pattern, handler)
 }
 
 func (engine *Engine) Run(add string) (err error) {
