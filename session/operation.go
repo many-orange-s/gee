c0cfd6882194032b029f312cbc8e766541163a51
many-orange-s <84865020+many-orange-s@users.noreply.github.com>
2021-11-14 19:17:10 +0800

    ADD:Engine 引入

--- session/operation.go
+++ session/operation.go
@@ -13,3 +13,18 @@ func (s *Session) Exec() (result sql.Result, err error) {
 	}
 	return result, err
 }
+
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars); err != nil {
+		log.Error(err)
+	}
+	return
+}
